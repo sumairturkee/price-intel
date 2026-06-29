bd30e7bd86cfc9e76b89facc414ba959d17b27c2
sumairturkee <sumair.turkee@gmail.com>
2026-06-29 11:48:17 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 export const metadata = { title: 'Price Intel', description: 'B2B Smartphone Price Tracker' };
 export default function RootLayout({ children }) {
-return (
-<html lang="en">
-<body style={{margin:0,padding:0,background:'#0D1117'}}>{children}</body>
-</html>
-);
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
