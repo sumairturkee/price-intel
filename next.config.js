9460103c4ae591f26016bf2d13eb34ce23bc5276
sumairturkee <sumair.turkee@gmail.com>
2026-06-29 16:17:19 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
-};
+const nextConfig = {};
 module.exports = nextConfig;
